59363f308738c4dc6d8fd50f9c91b713b162b212
Cedric G <62912213+Syfrost@users.noreply.github.com>
2025-03-18 07:53:34 +0100

    Update tm_check_validation.js

--- tampermonkey/tm_check_validation.js
+++ tampermonkey/tm_check_validation.js
@@ -44,6 +44,7 @@
     styleButton(buttonSign, 'orange', 'fa-pen');
     buttonSign.onclick = function() {
         const buttons_SIGN = document.querySelectorAll(`button[cp="${cpValue}"]`);
+console.log(${cpValue});
         buttons_SIGN.forEach(button => button.click());
     };
     buttonContainer.appendChild(buttonSign);
