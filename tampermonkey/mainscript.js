65beba00f09c03d371f899bdfa4ae08e47bc4a3b
Cedric G <62912213+Syfrost@users.noreply.github.com>
2025-03-21 12:14:11 +0100

    Update mainscript.js

--- tampermonkey/mainscript.js
+++ tampermonkey/mainscript.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Script collector auto
-// @namespace    http://tampermonkey.net/
+// @namespace    https://github.com/Syfrost/JustWork-Next-Extension
 // @version      1.2.4
 // @description  Charge plusieurs scripts distants
 // @author       Cedric G
@@ -8,4 +8,6 @@
 // @require      https://raw.githubusercontent.com/Syfrost/JustWork-Next-Extension/refs/heads/master/tampermonkey/tm_check_validation.js
 // @require      https://raw.githubusercontent.com/Syfrost/JustWork-Next-Extension/refs/heads/master/tampermonkey/tm_copy_cri.js
 // @require      https://raw.githubusercontent.com/Syfrost/JustWork-Next-Extension/refs/heads/master/tampermonkey/tm_copy_rex.js
+// @updateURL    https://raw.githubusercontent.com/Syfrost/JustWork-Next-Extension/refs/heads/master/tampermonkey/mainscript.js
+// @downloadURL  https://raw.githubusercontent.com/Syfrost/JustWork-Next-Extension/refs/heads/master/tampermonkey/mainscript.js
 // ==/UserScript==
