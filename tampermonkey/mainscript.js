5841847405111ca09b6b484499e140a2fff88592
Cedric G <62912213+Syfrost@users.noreply.github.com>
2025-03-21 12:05:22 +0100

    Create mainscript.js

---
+++ tampermonkey/mainscript.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name         Script collector auto
+// @namespace    http://tampermonkey.net/
+// @version      1.2.3
+// @description  Charge plusieurs scripts distants
+// @author       Cedric G
+// @match        https://prod.cloud-collectorplus.mt.sncf.fr/Prm/Reparation/*
+// @require      https://raw.githubusercontent.com/Syfrost/JustWork-Next-Extension/refs/heads/master/tampermonkey/tm_check_validation.js
+// @require      https://raw.githubusercontent.com/Syfrost/JustWork-Next-Extension/refs/heads/master/tampermonkey/tm_copy_cri.js
+// @require      https://raw.githubusercontent.com/Syfrost/JustWork-Next-Extension/refs/heads/master/tampermonkey/tm_copy_rex.js
+// ==/UserScript==
