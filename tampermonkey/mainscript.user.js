59b6c38c78ec22b44904a47d8e502f29fef4917b
Cedric G <62912213+Syfrost@users.noreply.github.com>
2025-03-24 12:12:26 +0100

    Update mainscript.user.js

--- tampermonkey/mainscript.user.js
+++ tampermonkey/mainscript.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Script collector auto
 // @namespace    https://github.com/Syfrost/JustWork-Next-Extension
-// @version      1.4.3
+// @version      1.4.4
 // @description  Charge plusieurs scripts distants
 // @author       Cedric G
 // @match        https://prod.cloud-collectorplus.mt.sncf.fr/Prm/Reparation/*
